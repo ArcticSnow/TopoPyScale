446a195a1abd456dfd5269bbae7d6ee4eabb2902
Joel Fiddes <joelfiddes@gmail.com>
2022-02-09 23:01:35 +0100

    Added Simons FSM export method.

--- TopoPyScale/topo_export.py
+++ TopoPyScale/topo_export.py
@@ -2,8 +2,9 @@
 Functions to export topo_scale output to formats compatible with existing models (e.g. CROCUS, Cryogrid, Snowmodel, ...)
 S. Filhol, December 2021
 
-TOSO;
+TODO;
 - export compressed netcdf ERROR!! syntax is not correct
+- SPHY forcing (grids)
 '''
 import sys
 
@@ -103,6 +104,41 @@ def to_cryogrid(ds,
         fo.to_netcdf(foutput, encoding=encod_dict)
         print('---> File {} saved'.format(foutput))
 
+def to_fsm(ds,
+            df_pts,
+            fname_format='FSM_pt_*.txt'):
+    '''
+    Function to export data for FSM.
+    format is a text file with the following columns
+    year month  day   hour  SW      LW      Sf         Rf     Ta  RH   Ua    Ps
+    (yyyy) (mm) (dd) (hh)  (W/m2) (W/m2) (kg/m2/s) (kg/m2/s) (K) (RH) (m/s) (Pa)
+    See README.md file from FSM source code for further details
+    TODO:
+    - Check unit
+    - Check format is compatible with compiled model
+    '''
+
+    n_digits = len(str(ds.point_id.values.max()))
+    for pt in ds.point_id.values:
+        foutput = fname_format.split('*')[0] + str(pt).zfill(n_digits) + fname_format.split('*')[1]
+        ds_pt = ds.sel(point_id=pt).copy()
+        df = pd.DataFrame()
+        df['year'] = pd.to_datetime(ds_pt.time.values).year
+        df['month']  = pd.to_datetime(ds_pt.time.values).month
+        df['day']  = pd.to_datetime(ds_pt.time.values).day
+        df['hr']  = pd.to_datetime(ds_pt.time.values).hour
+        df['SW'] = ds_pt.SW.values
+        df['LW'] = ds_pt.LW.values
+        rain, snow = mu.partition_snow(ds_pt.tp.values, ds_pt.t.values)
+        df['snowfall'] = snow / (60. * 60.)
+        df['rainfall'] = rain / (60. * 60.)
+        df['Tair'] = np.round(ds_pt.t.values, 2)
+        df['RH'] = mu.q_2_rh(ds_pt.t.values, ds_pt.p.values, ds_pt.q.values) * 100
+        df['speed'] = ds_pt.ws.values
+        df['p'] = ds_pt.p.values
+
+        df.to_csv(foutput, index=False, header=False, sep=' ')
+        print('---> File {} saved'.format(foutput))
 
 def to_micromet_single_station(ds,
                                df_pts,
