d032defc8397d455fb2d1539ba962235e5793952
arcticsnow <sfilhol@protonmail.com>
2022-10-24 15:06:45 +0200

    typo to to_crocus() RH computation

--- TopoPyScale/topo_export.py
+++ TopoPyScale/topo_export.py
@@ -318,11 +318,11 @@ def to_crocus(ds,
         df['DIR_SWdown'] = ds_pt.SW.values
         df['LWdown'] = ds_pt.LW.values
         df['PSurf'] = ds_pt.p.values
-        df['HUMREL'] = mu.q_2_rh(ds_pt.t.values - 273.15, ds_pt.p.values*10**-2, ds_pt.q.values) * 100
+        df['HUMREL'] = mu.q_2_rh(ds_pt.t.values, ds_pt.p.values, ds_pt.q.values)
         df['xwind'] = ds_pt.u.values
         df['ywind'] = ds_pt.v.values
         df['precip'] = ds_pt.tp.values / 3600 * scale_precip
-        rh = mu.q_2_rh(ds_pt.t.values, ds_pt.p.values, ds_pt.q.values)
+        rh = mu.q_2_rh(ds_pt.t.values, ds_pt.p.values, ds_pt.q.values)*100
         df['Rainf'], df['Snowf'] = mu.partition_snow(ds_pt.tp.values, ds_pt.t.values, rh, ds_pt.p.values, method=snow_partition_method)
 
         # Derive variables: Q- humidity, WD - wind direction (deg), and WS
