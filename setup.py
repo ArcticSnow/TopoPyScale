cc99ffc2eadb77eb0286c20ad4c646f750c10efa
arcticsnow <sfilhol@protonmail.com>
2023-02-27 10:31:42 +0100

    updating setup.py with version number corresponding to release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 setup(
     name='topopyscale',
-    version='0.1.7',
+    version='0.2.0',
     description='A Python package to perform climate downscaling at the hillslope scale',
     long_description=long_description,
 
