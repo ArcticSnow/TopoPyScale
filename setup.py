f514c3a421909c4bd7791e44535b3fb5cd634feb
arcticsnow <sfilhol@protonmail.com>
2023-03-08 14:38:33 +0100

    update to python 3.9 as xarray does not support 3.8 anymore: https://github.com/pydata/xarray/releases/tag/v2023.02.0

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # What does your project relate to?
