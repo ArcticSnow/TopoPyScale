6b4d006559b56267ecbea658dcdcaae0844c1018
arcticsnow <sfilhol@protonmail.com>
2024-03-14 15:31:27 +0100

    masking bug2

--- TopoPyScale/topoclass.py
+++ TopoPyScale/topoclass.py
@@ -416,7 +416,7 @@ class Topoclass(object):
             df_param['point_ind'] = df_param.cluster_labels.astype(int)
 
         # Build the final cluster map
-        self.toposub.ds_param['cluster_labels'] = (["y", "x"], np.reshape(df_param.cluster_labels.astype(int).values, self.toposub.ds_param.slope.shape))
+        self.toposub.ds_param['cluster_labels'] = (["y", "x"], np.reshape(df_param.point_name.values, self.toposub.ds_param.slope.shape))
         self.toposub.ds_param['point_name'] = (["y", "x"], np.reshape(df_param.point_name.values, self.toposub.ds_param.slope.shape))
         self.toposub.ds_param['point_ind'] = (["y", "x"], np.reshape(df_param.point_ind.values, self.toposub.ds_param.slope.shape))
 
