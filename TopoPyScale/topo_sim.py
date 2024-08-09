56030aea6477db2508d5614717b8cfdd6bc986ef
Joel Fiddes <joelfiddes@gmail.com>
2024-08-09 10:12:53 +0200

    fixed topo_sim issue of returning lon or lat vectors that ere one pixel out

--- TopoPyScale/topo_sim.py
+++ TopoPyScale/topo_sim.py
@@ -664,11 +664,16 @@ def topo_map_forcing(ds_var, n_decimals=2, dtype='float32', new_res=None):
         #     resampling=Resampling.nearest
         # )
 
-    # return coords of resampled grid here (this does not preserve dimensions perfectly (can be 1pix out))
+
+         # return coords of resampled grid here (this does not preserve dimensions perfectly (can be 1pix out))
         array = src.read()
         min_E, min_N, max_E, max_N = src.bounds
-        lons = np.arange(min_E, max_E, src.res[0])
-        lats = np.arange(min_N, max_N, src.res[1])
+        #lons = np.arange(min_E, max_E, src.res[0])
+        #lats = np.arange(min_N, max_N, src.res[1])
+        #lats = lats[::-1]
+
+        lons = np.linspace(min_E, max_E, src.width)
+        lats = np.linspace(min_N, max_N, src.height)
         lats = lats[::-1]
 
     array2 = lookup2D.transpose()[array]  # Reclassify in a single operation using broadcasting
