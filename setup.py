70a41d32c5c61a4308beb2f2b3b33ea91b038e15
arcticsnow <sfilhol@protonmail.com>
2025-05-05 16:00:28 +0200

    upgrade version after PR by @mmazzolini

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 setup(
     name='topopyscale',
-    version='0.2.6',
+    version='0.2.7',
     description='TopoPyScale: A Python Package for Hillslope Climate Downscaling',
     long_description=long_description,
 
