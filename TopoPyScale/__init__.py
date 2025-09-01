cf2fe6698a3edcadc32bfbffed97c6a5b2f9aaa0
arcticsnow <sfilhol@protonmail.com>
2025-09-01 10:43:05 +0200

    updating to new Pipy version and package handling with flit

---
+++ TopoPyScale/__init__.py
@@ -0,0 +1,10 @@
+"""
+TopoPyScale, a Python package to perform simplistic climate downscaling at the hillslope scale.
+
+This package provides a number of routine to, in-fine, realize climate downscaling using simplistic methods such as vertical and horizontal interpolation and geometric corrections. TPS allows to segment DEM with k-means routine integrated int the workflow to reduce computation and maintain some good spatial granularity.
+
+Example usage: See the documentation
+
+"""
+
+__version__ = "0.3.0"
